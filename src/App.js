32f2ddd232cb1852e16680fed5421629608ea1d9
pranjal doorwar <doorwarpranjal@gmail.com>
2021-03-06 13:57:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Homepage from './MainApp/Homepage_folder/Homepage'
+import Navbar from './MainApp/Navbar_folder/Navbar'
+import Footer from './MainApp/Footer_folder/Footer'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+<Navbar/>
+
+<Homepage/>
+   
+
+   <Footer/>
     </div>
   );
 }
---
+++ src/MainApp/Homepage_folder/Homepage.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+export default function Homepage() {
+    return (
+        <div>
+
+
+
+             {/* remove this section before coding */}
+            <br/>
+      <center><h2>Start your code from here</h2></center> 
+      <br/>
+      {/* remove this section before coding */}
+
+
+
+
+
+
+
+
+
+
+
+
+      
+        </div>
+    )
+}
