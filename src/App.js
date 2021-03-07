fa5e2a170b360955d4661a282cb9e1b31a0bcf7b
Anurag Singh <you@example.com>
2021-03-07 22:59:19 +0530

    Add tours pages

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 import NotFound from "./components/NotFound/Not-found";
 import Contact from "./components/Contact-us/contact-us";
+import Destination from "./components/Destination/Destination";
+import DestinationDetails from "./components/Destination/Destination-Details";
 import LoginScreen from "./components/Auth/LoginScreen"
+import Tours from "./components/Tours/Tours"
+import TripOffers from "./components/Tours/TripOffers"
 import RegisterScreen from "./components/Auth/RegisterScreen"
 import ForgotPasswordScreen from "./components/Auth/ForgotPasswordScreen"
 function App() {
@@ -16,10 +20,12 @@ function App() {
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/about-us" exact component={About} />
-          <Route path="/login" exact component={LoginScreen} />
+          <Route path="/sign-in" exact component={LoginScreen} />
           <Route path="/register" exact component={RegisterScreen} />
           <Route path="/forgot-password" exact component={ForgotPasswordScreen} />
           <Route path="/contact" component={Contact} />
+          <Route path="/destinations" component={Destination} />
+          <Route path="/destination-details" component={DestinationDetails} />
           <Route path="*" component={NotFound} />
         </Switch>
       </Router>
